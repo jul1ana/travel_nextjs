a509e5aaf419ca9c7b20be1934cb9872b88d1cd6
jul1ana <julianaadt97@gmail.com>
2024-04-30 16:35:15 -0300

    feat: add secao de hero

--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -1,17 +1,24 @@
 import Link, { LinkProps } from "next/link";
 import { ReactNode } from "react";
 import { tv, VariantProps } from "tailwind-variants";
+import { FaArrowRight } from "react-icons/fa";
 
 const link = tv({
-  base: 'flex items-center justify-center gap-3 border border-[#086ad7] rounded bg-[#086ad7] px-6 py-4 lg:px-10 lg:py-5 text-white transition-all hover:bg-[#211e3b] hover:border hover:border-[#211e3b]',
+  base: 'flex items-center justify-center gap-3 border border-[#086ad7] rounded bg-[#086ad7] px-6 py-4 lg:px-10 lg:py-5 text-sm text-white uppercase font-semibold transition-all hover:bg-[#211e3b] hover:border hover:border-[#211e3b]',
   variants: {
     size: {
-      default: 'h-8 px-15',
-      sm: 'h-40 px-2'
+      default: 'px-2 py-4',
+      sm: 'px-2 py-4 w-1/2'
+    },
+    theme: {
+      principal: '',
+      minimal: 'text-black bg-transparent',
+      off: '',
     },
   },
   defaultVariants: {
     size: 'default',
+    theme: 'principal'
   }
 })
 
@@ -19,10 +26,11 @@ export type CustomLinkProps = LinkProps & VariantProps<typeof link> & {
   children: ReactNode;
 };
 
-export function CustomLink({ children, size, ...props }: CustomLinkProps) {
+export function CustomLink({ children, size, theme, ...props }: CustomLinkProps) {
   return (
-    <Link className={link({size})} {...props}>
+    <Link className={link({ size, theme })} {...props}>
       {children}
+      <FaArrowRight size={14} />
     </Link>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,16 +3,14 @@ import { CustomLink } from "./CustomLink";
 export function Hero() {
   return (
     <section className="mx-auto max-w-[1440px] px-6 lg:px-20 3xl:px-0">
-      <div className="space-y-3">
+      <div className="space-y-3 text-left">
         <h2 className="text-4xl font-bold text-[#211E3B]">Solutions For</h2>
         <h1 className="text-5xl font-bold text-[#211E3B]">IT Business</h1>
         <p className="text-lg text-[#211E3B] font-semibold">We Have 25 Years Of Experience In IT Solutions</p>
 
-        <div className="space-y-4">
-          <CustomLink href='/' size="sm">TESTE</CustomLink>
-          <CustomLink href='/'>TESTE</CustomLink>
-          <CustomLink href='/'>TESTE</CustomLink>
-          <CustomLink href='/'>TESTE</CustomLink>
+        <div className="space-y-4 flex flex-col w-full">
+          <CustomLink href='/' size="default">Service we provide</CustomLink>
+          <CustomLink href='/' size="sm" theme="minimal">learn more</CustomLink>
         </div>
       </div>
 
