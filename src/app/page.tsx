43e2813d7a3546bbb3f30ab9bd0a0a07195827a1
jul1ana <julianaadt97@gmail.com>
2024-05-02 12:00:25 -0300

    fix: edit espacamento /Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Hero } from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="space-y-44 my-20">
+    <main className="space-y-20 my-10">
       <Hero />
       <Business />
       <About />
