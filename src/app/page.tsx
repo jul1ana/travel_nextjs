ed3f95dc255417bc7d4d8fe1aee2843ba366943e
jul1ana <julianaadt97@gmail.com>
2024-05-02 13:54:08 -0300

    feat: add section Services

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { About } from "@/components/About";
 import { Business } from "@/components/Business";
 import { Hero } from "@/components/Hero";
+import { Services } from "@/components/Services";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Business />
       <About />
+      <Services />
     </main>
   );
 }
---
+++ src/components/Services.tsx
@@ -0,0 +1,62 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FaArrowRight } from "react-icons/fa";
+
+export function Services() {
+  return (
+    <section className="bg-[#211e3b]">
+      <div className="mx-auto max-w-[1440px] px-6 py-20 lg:px-20 lg:py-48 3xl:px-0">
+        <div className="flex flex-col items-center space-y-16 lg:space-y-32">
+          {/* CONTENT */}
+          <div className="text-white text-center space-y-4 lg:space-y-0">
+            <p className="uppercase font-semibold text-base">Popular IT services</p>
+            <h1 className="lg:text-5xl text-3xl font-bold">Our Professional Solutions <br />For IT Business</h1>
+          </div>
+
+          {/* CARDS */}
+          <div className="flex flex-col gap-8 lg:flex-row">
+            <div className="border border-[#312e49] font-semibold flex flex-col items-center py-12 px-2 space-y-4 rounded-md hover:bg-[#2e2a50] hover:border-[#2e2a50]">
+              <Image src="/sicon1.png" alt="Serviços TI" width={140} height={140} />
+              <h4 className="text-white">Manage IT Services</h4>
+              <p className="text-[#9a95c0] w-4/5 text-center">Sed perspicias unde omnis natus error volute</p>
+              <Link href="/" className="text-[#9a95c0] uppercase flex items-center gap-4">
+                learn more
+                <FaArrowRight size={14} />
+              </Link>
+            </div>
+
+            <div className="border border-[#312e49] font-semibold flex flex-col items-center py-12 px-2 space-y-4 rounded-md hover:bg-[#2e2a50] hover:border-[#2e2a50]">
+              <Image src="/sicon2.png" alt="Serviços TI" width={140} height={140} />
+              <h4 className="text-white">Product Engineering</h4>
+              <p className="text-[#9a95c0] w-4/5 text-center">Sed perspicias unde omnis natus error volute</p>
+              <Link href="/" className="text-[#9a95c0] uppercase flex items-center gap-4">
+                learn more
+                <FaArrowRight size={14} />
+              </Link>
+            </div>
+
+            <div className="border border-[#312e49] font-semibold flex flex-col items-center py-12 px-2 space-y-4 rounded-md hover:bg-[#2e2a50] hover:border-[#2e2a50]">
+              <Image src="/sicon3.png" alt="Serviços TI" width={140} height={140} />
+              <h4 className="text-white">Web Development</h4>
+              <p className="text-[#9a95c0] w-4/5 text-center">Sed perspicias unde omnis natus error volute</p>
+              <Link href="/" className="text-[#9a95c0] uppercase flex items-center gap-4">
+                learn more
+                <FaArrowRight size={14} />
+              </Link>
+            </div>
+
+            <div className="border border-[#312e49] font-semibold flex flex-col items-center py-12 px-2 space-y-4 rounded-md hover:bg-[#2e2a50] hover:border-[#2e2a50]">
+              <Image src="/sicon4.png" alt="Serviços TI" width={140} height={140} />
+              <h4 className="text-white">Digital Solutions</h4>
+              <p className="text-[#9a95c0] w-4/5 text-center">Sed perspicias unde omnis natus error volute</p>
+              <Link href="/" className="text-[#9a95c0] uppercase flex items-center gap-4">
+                learn more
+                <FaArrowRight size={14} />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
