e3b9b63aae66e8b105c714ea12a91b3dcacc3941
jul1ana <julianaadt97@gmail.com>
2024-05-03 08:28:44 -0300

    fix: edit espacamento mobile/desktop em Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import { CustomLink } from "./CustomLink";
 export function Hero() {
   return (
     <section className="mx-auto max-w-[1440px] px-6 lg:px-20 3xl:px-0 mt-20">
-      <div className="space-y-3 text-left lg:space-y-4">
+      <div className="space-y-4 text-left lg:space-y-8">
         <h2 className="text-4xl font-bold text-[#211E3B] lg:text-7xl">Solutions For</h2>
         <h1 className="text-5xl font-bold text-[#211E3B] lg:text-9xl">IT Business</h1>
         <p className="text-lg text-[#211E3B] font-semibold lg:text-2xl">We Have 25 Years Of Experience In IT Solutions</p>
