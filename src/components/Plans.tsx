21e55aebd9fbba4e58b6975b29e26290f245e12f
jul1ana <julianaadt97@gmail.com>
2024-05-02 21:07:07 -0300

    feat: add section Plans

--- src/components/Plans.tsx
+++ src/components/Plans.tsx
@@ -14,13 +14,13 @@ export function Plans() {
         {/* CARDS */}
         <div className="flex gap-14">
           <div className="border-2 border-[#312e49] p-10 space-y-10 rounded-md flex flex-col items-start">
-            <div className="font-semibold">
+            <div className="font-semibold space-y-4">
               <h3 className="text-xl text-white">Basic Plan</h3>
-              <p className="text-[#9a95c0]">Quis autem vel eum iure reprehes derit quin voluptate velite</p>
+              <p className="text-[#9a95c0] leading-6">Quis autem vel eum iure reprehes derit quin voluptate velite</p>
               <p className="text-2xl text-white">$25.63 <span className="text-base text-[#9a95c0]">monthly</span></p>
             </div>
 
-            <ul className="text-base font-semibold text-[#9a95c0]">
+            <ul className="space-y-2 text-base font-semibold text-[#9a95c0]">
               <li className="flex items-center gap-3">
                 <FaCheck />
                 IT Consulting
@@ -39,32 +39,50 @@ export function Plans() {
           </div>
 
           <div className="border-2 border-[#312e49] p-10 space-y-10 rounded-md flex flex-col items-start">
-            <div className="font-semibold">
+            <div className="font-semibold space-y-4">
               <h3 className="text-xl text-white">Basic Plan</h3>
-              <p className="text-[#9a95c0]">Quis autem vel eum iure reprehes derit quin voluptate velite</p>
+              <p className="text-[#9a95c0] leading-6">Quis autem vel eum iure reprehes derit quin voluptate velite</p>
               <p className="text-2xl text-white">$25.63 <span className="text-base text-[#9a95c0]">monthly</span></p>
             </div>
 
-            <ul className="text-base font-semibold text-[#9a95c0]">
-              <li>IT Consulting</li>
-              <li>Product Engineering</li>
-              <li>Digital Solutions</li>
+            <ul className="space-y-2 text-base font-semibold text-[#9a95c0]">
+              <li className="flex items-center gap-3">
+                <FaCheck />
+                IT Consulting
+              </li>
+              <li className="flex items-center gap-3">
+                <FaCheck />
+                Product Engineering
+              </li>
+              <li className="flex items-center gap-3">
+                <FaCheck />
+                Digital Solutions
+              </li>
             </ul>
 
             <CustomLink href='/' size="default">get started</CustomLink>
           </div>
 
           <div className="border-2 border-[#312e49] p-10 space-y-10 rounded-md flex flex-col items-start">
-            <div className="font-semibold">
+            <div className="font-semibold space-y-4">
               <h3 className="text-xl text-white">Basic Plan</h3>
-              <p className="text-[#9a95c0]">Quis autem vel eum iure reprehes derit quin voluptate velite</p>
+              <p className="text-[#9a95c0] leading-6">Quis autem vel eum iure reprehes derit quin voluptate velite</p>
               <p className="text-2xl text-white">$25.63 <span className="text-base text-[#9a95c0]">monthly</span></p>
             </div>
 
-            <ul className="text-base font-semibold text-[#9a95c0]">
-              <li>IT Consulting</li>
-              <li>Product Engineering</li>
-              <li>Digital Solutions</li>
+            <ul className="space-y-2 text-base font-semibold text-[#9a95c0]">
+              <li className="flex items-center gap-3">
+                <FaCheck />
+                IT Consulting
+              </li>
+              <li className="flex items-center gap-3">
+                <FaCheck />
+                Product Engineering
+              </li>
+              <li className="flex items-center gap-3">
+                <FaCheck />
+                Digital Solutions
+              </li>
             </ul>
 
             <CustomLink href='/' size="default">get started</CustomLink>
