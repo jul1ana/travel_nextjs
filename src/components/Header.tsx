2f4239abeeb7a40aefec9fab987c186019f0720c
jul1ana <julianaadt97@gmail.com>
2024-05-03 11:56:12 -0300

    fix: edit menu/mobile em Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,52 +1,68 @@
-'use client'
-import Link from 'next/link';
-import Image from 'next/image';
-import { NAV_LINKS } from '@/constants';
-import { useState } from 'react';
-import { CustomLink } from './CustomLink';
+"use client"
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { FaRegCircleXmark } from "react-icons/fa6";
+import { RiMenu3Line } from "react-icons/ri";
+import { CustomLink } from "./CustomLink";
 
 export function Header() {
+
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const navItems = [
+    { link: "Home", path: "home" },
+    { link: "About", path: "about" },
+    { link: "Services", path: "services" },
+    { link: "Pages", path: "pages" },
+    { link: "News", path: "news" },
+    { link: "Contact", path: "contact" },
+  ];
+
   return (
     <header>
-      <nav className="flex items-center justify-between mx-auto max-w-[1440px] px-6 lg:px-20 3xl:px-0 relative z-30 py-5">
-        <Link href="/" className="lg:w-52">
-          <Image src="/logo.png" alt="Logo" width={100} height={29} />
-        </Link>
-
-        <ul className={`hidden h-full gap-12 lg:flex ${isMenuOpen ? "block" : "hidden"}`}>
-          {NAV_LINKS.map((link) => (
-            <Link
-              href={link.href}
-              key={link.key}
-              className="text-[16px] font-[500] text-black flex items-center justify-center cursor-pointer pb-1.5 transition-all hover:font-bold"
-            >
-              {link.label}
+      <nav className="max-w-screen-2xl mx-auto md:px-14 p-4 bg-white text-primary border-b lg:border-b-0 z-50 fixed lg:static top-0 right-0 left-0">
+        <div className="container mx-auto flex justify-between items-center text-lg font-medium">
+          <div className="flex space-x-14 items-center">
+            <Link href="/" className="lg:w-52">
+              <Image src="/logo.png" alt="Logo" width={100} height={29} />
             </Link>
-          ))}
-        </ul>
 
-        <div className="hidden lg:flex lg:items-center lg:justify-center">
-          <CustomLink href="/" size="default">
-            CONSTANCY
-          </CustomLink>
+            {/* NAV ITEMS */}
+            <ul className="md:flex space-x-12 hidden">
+              {
+                navItems.map(({ link, path }) => <Link key={link} href={path} className="block hover:text-yellow-500"> {link} </Link>)
+              }
+            </ul>
+          </div>
+
+          {/* BTN */}
+          <div className="space-x-12 hidden md:flex items-center">
+            <CustomLink href='/' size="default">constancy</CustomLink>
+          </div>
+
+          {/* MENU MOBILE */}
+          <div className="md:hidden">
+            <button onClick={toggleMenu} className="text-white focus:outline-none focus:text-gray-300">
+              {
+                isMenuOpen ?
+                  (<FaRegCircleXmark className="w-6 h-6 text-blue-700" />) :
+                  (<RiMenu3Line className="w-6 h-6 text-blue-700" />)
+              }
+            </button>
+          </div>
         </div>
-
-        <button onClick={toggleMenu}>
-          <Image
-            src="/menu-aberto.png"
-            alt="Menu"
-            width={32}
-            height={32}
-            className="inline-block cursor-pointer lg:hidden"
-          />
-        </button>
       </nav>
+
+      <div className={`space-y-6 px-8 pt-24 pb-5 bg-[#086ad7] h-full font-semibold text-[#FFF] text-lg ${isMenuOpen ? "block fixed top-0 right-0 left-0" : "hidden"}`}>
+        {
+          navItems.map(({ link, path }) => <Link key={link} href={path} className="block hover:text-gray-300 p-2 border-b border-b-gray-400/30"> {link} </Link>)
+        }
+      </div>
     </header>
   );
 };
