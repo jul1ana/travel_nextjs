6cf79f0c5ddaac84f9dda007a5fe413863458cf1
jul1ana <julianaadt97@gmail.com>
2024-05-03 08:25:02 -0300

    fix: edit estilizacao em Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,50 +9,51 @@ export function Footer() {
   return (
     <footer className="mx-auto max-w-[1440px] space-y-6 px-8 py-16 lg:px-20 3xl:px-0">
       <div className="flex flex-col lg:flex-row gap-10 lg:w-full justify-items-start">
-        <div className="lg:w-1/4 flex flex-col gap-4">
+        <div className="lg:w-1/4 flex flex-col gap-10">
           <Link href="/" className="lg:w-52">
             <Image src="/logo.png" alt="Logo" width={100} height={29} />
           </Link>
 
           <p className="text-[#696969] font-semibold">Sed ut perspiciatis unde omnis natus voluptatem accusa ntiumy doloremque laudantium.</p>
 
-          <div>
-            <input placeholder="Email Address" />
-            <button>
-              <HiOutlineMailOpen />
+          <div className="flex items-center">
+            <input placeholder="Email Address" className="bg-[#F5F5F5] p-4 font-semibold rounded-l-lg" />
+
+            <button className="bg-[#086AD7] p-4 rounded-r-lg">
+              <HiOutlineMailOpen color="#FFF" size={24} />
             </button>
           </div>
         </div>
 
-        <div className="lg:w-1/4 space-y-4">
-          <h4 className="text-[#211E3B] font-semibold">Company</h4>
+        <div className="lg:w-1/4 space-y-8">
+          <h4 className="text-[#211E3B] font-semibold lg:text-2xl">Company</h4>
 
-          <ul className="space-y-4">
+          <ul className="space-y-4 text-[#696969] font-semibold">
             <li className="flex items-center gap-2">
               <MdKeyboardDoubleArrowRight />
-              <Link href="/">About Us</Link>
+              <Link href="/" className="hover:translate-x-2 transition-all duration-300 hover:text-[#086AD7]">About Us</Link>
             </li>
             <li className="flex items-center gap-2">
               <MdKeyboardDoubleArrowRight />
-              <Link href="/">About Us</Link>
+              <Link href="/" className="hover:translate-x-2 transition-all duration-300 hover:text-[#086AD7]">Company History</Link>
             </li>
             <li className="flex items-center gap-2">
               <MdKeyboardDoubleArrowRight />
-              <Link href="/">About Us</Link>
+              <Link href="/" className="hover:translate-x-2 transition-all duration-300 hover:text-[#086AD7]">Need A Career</Link>
             </li>
             <li className="flex items-center gap-2">
               <MdKeyboardDoubleArrowRight />
-              <Link href="/">About Us</Link>
+              <Link href="/" className="hover:translate-x-2 transition-all duration-300 hover:text-[#086AD7]">Working Process</Link>
             </li>
             <li className="flex items-center gap-2">
               <MdKeyboardDoubleArrowRight />
-              <Link href="/">About Us</Link>
+              <Link href="/" className="hover:translate-x-2 transition-all duration-300 hover:text-[#086AD7]">Blog Post</Link>
             </li>
           </ul>
         </div>
 
-        <div className="lg:w-1/4 space-y-4">
-          <h4 className="text-[#211E3B] font-semibold">News News</h4>
+        <div className="lg:w-1/4 space-y-8">
+          <h4 className="text-[#211E3B] font-semibold lg:text-2xl">News News</h4>
 
           <div className="space-y-10">
             <div className="flex gap-4">
@@ -60,7 +61,7 @@ export function Footer() {
 
               <div className="font-semibold ">
                 <span className="flex items-center gap-1 text-[#696969]"> <FaRegCalendarAlt /> 24th Nov 2020</span>
-                <p className="text-[#211E3B]">User’s Perspes Using Story Structure</p>
+                <p className="text-[#211E3B] hover:text-[#086AD7]">User’s Perspes Using Story Structure</p>
               </div>
             </div>
 
@@ -69,14 +70,14 @@ export function Footer() {
 
               <div className="font-semibold ">
                 <span className="flex items-center gap-1 text-[#696969]"> <FaRegCalendarAlt /> 24th Nov 2020</span>
-                <p className="text-[#211E3B]">User’s Perspes Using Story Structure</p>
+                <p className="text-[#211E3B] hover:text-[#086AD7]">User’s Perspes Using Story Structure</p>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="lg:w-1/4 space-y-4">
-          <h4 className="text-[#211E3B] font-semibold">Contact Us</h4>
+        <div className="lg:w-1/4 space-y-8">
+          <h4 className="text-[#211E3B] font-semibold lg:text-2xl">Contact Us</h4>
 
           <div className="flex items-center gap-2">
             <FaLocationDot color="#086AD7" />
