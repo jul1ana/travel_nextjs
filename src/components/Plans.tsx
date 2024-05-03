2bb17cca7bf64469f10716ca87376c4acb6cc90a
jul1ana <julianaadt97@gmail.com>
2024-05-02 21:17:52 -0300

    fix: responsividade no componente Plans

--- src/components/Plans.tsx
+++ src/components/Plans.tsx
@@ -4,15 +4,15 @@ import { FaCheck } from "react-icons/fa6";
 export function Plans() {
   return (
     <section className="bg-[#211e3b]">
-      <div className="mx-auto max-w-[1440px] px-6 py-20 lg:px-20 lg:py-48 3xl:px-0 lg:space-y-20">
+      <div className="mx-auto max-w-[1440px] px-6 py-20 lg:px-20 lg:py-48 3xl:px-0 space-y-20">
         {/* TITLE */}
         <div className="text-white text-center">
           <p className="uppercase font-semibold text-base">OUR PRICING PLAN</p>
-          <h1 className="lg:text-5xl text-3xl font-bold">Awesome Pricing Plan</h1>
+          <h1 className="lg:text-5xl text-2xl font-bold">Awesome Pricing Plan</h1>
         </div>
 
         {/* CARDS */}
-        <div className="flex gap-14">
+        <div className="flex flex-col lg:flex-row gap-14">
           <div className="border-2 border-[#312e49] p-10 space-y-10 rounded-md flex flex-col items-start">
             <div className="font-semibold space-y-4">
               <h3 className="text-xl text-white">Basic Plan</h3>
